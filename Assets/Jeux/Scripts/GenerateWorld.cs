debdf92cd569f9aae4c3fd7cd01b6bea694760f8
CaniculeGame <florian.raffalli@gmail.com>
2019-07-24 23:23:53 +0200

    amelioration creation route infinie

--- Assets/Jeux/Scripts/GenerateWorld.cs
+++ Assets/Jeux/Scripts/GenerateWorld.cs
@@ -5,39 +5,40 @@ using UnityTest;
 
 public class GenerateWorld : MonoBehaviour
 {
+    /* ground principal */
     public GameObject groundObj;    // objet a repeter
     public GameObject groundParent; // parent qui contiendra tous les objets à repeter
+    private PoolObjects groundObjectPool; // pool de ground
 
-    public Transform   lastGenerated; //on garde la position de la derniere plateforme 
-    public TimeManager timeMnager;    //ralentie le temps 
+    public TimeManager timeMnager;    //gestion dsu slow motion 
     
     private GameVar game;
-    private PoolObjects groundObjectPool;
+
 
     // Start is called before the first frame update
     void Start()
     {
         game = GameVar.DonnerInstance();
+
+        // placement des premiers block 
         groundObjectPool = new PoolObjects();
         groundObjectPool.SetGameObject = groundObj;
         groundObjectPool.SetParentGameObject = groundParent.transform;
-        groundObjectPool.CreerObject(new Vector3(0, 0, 3), Quaternion.identity);
-        groundObjectPool.CreerObject(new Vector3(30.3f, 0, 3), Quaternion.identity);
-
+        groundObjectPool.CreerObject(new Vector3(0, -0.5f, 3), Quaternion.identity);
     }
 
-
+    /* A DEPLACER DANS GAMEPLAY , ICI ON FAIT QUE LE MONDE  */
     private void FixedUpdate()
     {
         float distanceParcourue = game.DistanceParcourue;
-        if (distanceParcourue > 50 && distanceParcourue  < 55 )
+        if (distanceParcourue > 250 && distanceParcourue  < 255 )
         {
             //passage en slowmotion pour tous le monde
             timeMnager.DoSlowmotion();
 
             // changement de gameplay
         }
-        else if (distanceParcourue > 55)
+        else if (distanceParcourue > 255)
         {
             //sortie du slowmotion
             timeMnager.StopSlowMotion();
@@ -46,14 +47,20 @@ public class GenerateWorld : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerExit(Collider other)
     {
         /* repete le sol de façon infinie*/
         if (other.tag == "GroundEnd")
         {
-            Transform savePos = other.transform;
-            groundObjectPool.CreerObject(savePos.position, Quaternion.identity);
             groundObjectPool.SupprimerObject(other.transform.parent.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "GroundEnd")
+        {
+            groundObjectPool.CreerObject(other.transform.position, Quaternion.identity);
         }
     }
 }
