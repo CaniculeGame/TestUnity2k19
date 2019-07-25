cbf4532435dc2e519c9dc650bfd325e18de78040
CaniculeGame <florian.raffalli@gmail.com>
2019-07-25 22:06:51 +0200

    ajout free asset + utilisation perlin

--- Assets/Jeux/Scripts/GenerateWorld.cs
+++ Assets/Jeux/Scripts/GenerateWorld.cs
@@ -10,6 +10,14 @@ public class GenerateWorld : MonoBehaviour
     public GameObject groundParent; // parent qui contiendra tous les objets à repeter
     private PoolObjects groundObjectPool; // pool de ground
 
+
+    public GameObject[] treedObj;
+    private PoolObjects treeObjectPool; // pool d'arbre
+    public  GameObject treeParent; // parent qui contiendra tous les objets à repeter
+
+
+    private PerlinNoiseGenerator perlinNoiseGenerator; // fabrique et concerve une map 2d avec le bruit de perlin 
+
     public TimeManager timeMnager;    //gestion dsu slow motion 
     
     private GameVar game;
@@ -25,11 +33,23 @@ public class GenerateWorld : MonoBehaviour
         groundObjectPool.SetGameObject = groundObj;
         groundObjectPool.SetParentGameObject = groundParent.transform;
         groundObjectPool.CreerObject(new Vector3(0, -0.5f, 3), Quaternion.identity);
+
+
+        treeObjectPool = new PoolObjects();
+        treeObjectPool.SetGameObject = treedObj[0];
+        treeObjectPool.SetParentGameObject = treeParent.transform;
+
+        perlinNoiseGenerator = new PerlinNoiseGenerator();
+        perlinNoiseGenerator.CalculatePerlinNoise();
+
     }
 
     /* A DEPLACER DANS GAMEPLAY , ICI ON FAIT QUE LE MONDE  */
     private void FixedUpdate()
     {
+        if (game == null)
+            game = GameVar.DonnerInstance();
+
         float distanceParcourue = game.DistanceParcourue;
         if (distanceParcourue > 250 && distanceParcourue  < 255 )
         {
@@ -49,18 +69,45 @@ public class GenerateWorld : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        /* repete le sol de façon infinie*/
-        if (other.tag == "GroundEnd")
+        if(other.tag == "GroundEnd")
         {
-            groundObjectPool.SupprimerObject(other.transform.parent.gameObject);
+            groundObjectPool.SupprimerObject(other.transform.parent.gameObject); // ground end est fils
         }
+        else if(other.tag == "Tree")
+        {
+            treeObjectPool.SupprimerObject(other.transform.gameObject); // tree est deja le parent
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "GroundEnd")
         {
+            // generation de la route
             groundObjectPool.CreerObject(other.transform.position, Quaternion.identity);
+
+            // generation du decors
+            Vector3 positionDebut = other.transform.position;
+            float size = groundObj.GetComponentInChildren<Renderer>().bounds.size.x;
+            Vector3 positionFin = new Vector3(positionDebut.x + size, positionDebut.y, positionDebut.z);
+
+            //quand on cree un chemin on va placer des objets le long du terrain precedement generé 
+            /*    perlinNoiseGenerator.PositionPerlinMap = new Vector2(Random.Range(0, perlinNoiseGenerator.Largeur), Random.Range(1,perlinNoiseGenerator.Hauteur - size));
+                for (int x = 0; x < size; x++)
+                {
+                    Vector2 position = perlinNoiseGenerator.PositionPerlinMap;
+                    Vector3 positionNouveauObj = other.transform.position;
+                    float val = perlinNoiseGenerator.ValeurPosition;
+
+                    perlinNoiseGenerator.PositionPerlinMap = new Vector2(position.x, position.y + 1);  //OnTriggerEnter incremente la position
+
+                    if (val*10 > 2f)
+                    {
+                        positionNouveauObj = new Vector3(position.x + Random.Range(0,size), Random.Range(0.1f, 1f), Random.Range(0.1f, 1f));
+                        treeObjectPool.CreerObject(other.transform.position, Quaternion.identity);
+                    }
+                }*/
         }
     }
 }
---
+++ Assets/PerlinNoiseGenerator.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class PerlinNoiseGenerator 
+{
+    private int v_hauteur;
+    private int v_largeur;
+    private float v_echelle;
+
+    private int v_offsetX;
+    private int v_offsetY;
+
+    private float[,] perlinMap;
+    private Vector2 positionPerlinMap;
+
+    public PerlinNoiseGenerator()
+    {
+        v_hauteur = 256;
+        v_largeur = 256;
+        v_echelle = 20f;
+
+        v_offsetX = 0;
+        v_offsetY = 0;
+
+        positionPerlinMap = new Vector2(0, 0);
+    }
+
+    public PerlinNoiseGenerator(int largeur, int hauteur, int offsetX, int offsetY, int echelle, bool calculate =false)
+    {
+        v_hauteur = hauteur;
+        v_largeur = largeur;
+        v_echelle = echelle;
+
+        v_offsetX = offsetX;
+        v_offsetY = offsetY;
+
+        if (calculate)
+            CalculatePerlinNoise();
+    }
+
+    public float[,] CalculatePerlinNoise()
+    {
+        float coordX = 0;
+        float coordY = 0;
+
+        perlinMap = new float[v_largeur, v_hauteur];
+
+        for (int x = 0; x < v_largeur; x++)
+        {
+            for (int y = 0; y < v_hauteur; y++)
+            {
+                coordX = (float)x / v_largeur * v_echelle;
+                coordY = (float)y / v_hauteur * v_echelle;
+                perlinMap[x, y] = Mathf.PerlinNoise(coordX, coordY);
+            }
+        }
+
+        return perlinMap;
+    }
+
+    public float[,] CalculatePerlinNoise(int largeur, int hauteur)
+    {
+        v_largeur = largeur;
+        v_hauteur = hauteur;
+
+        return CalculatePerlinNoise();
+    }
+
+    public float[,] RegenerateMap()
+    {
+        return CalculatePerlinNoise();
+    }
+
+
+    public float[,] RegenerateMap(int largeur, int hauteur)
+    {
+        v_largeur = largeur;
+        v_hauteur = hauteur;
+
+        return CalculatePerlinNoise(largeur, hauteur);
+    }
+
+    public int Hauteur { get { return v_hauteur; } set { v_hauteur = value; } }
+    public int Largeur { get { return v_largeur; } set { v_largeur = value; } }
+    public int OffsetX { get { return v_offsetX; } set { v_offsetX = value; } }
+    public int OffsetY { get { return v_offsetY; } set { v_offsetY = value; } }
+    public float Echelle { get { return v_echelle; } set { v_echelle = value; } }
+    public float[,] PerlinMap { get { return perlinMap; } }
+    public Vector2 PositionPerlinMap { get { return positionPerlinMap; } set { positionPerlinMap = value; } }
+    public float ValeurPosition { get{ return perlinMap[(int)positionPerlinMap.x, (int)positionPerlinMap.y]; } }
+}
