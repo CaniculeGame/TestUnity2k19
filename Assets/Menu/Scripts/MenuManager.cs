f668b2958c7d0843b3653b78d306b0e9b20e8766
CaniculeGame <florian.raffalli@gmail.com>
2019-07-30 21:39:40 +0200

    reorganisation menu principal + ajout ressource

--- Assets/Menu/Scripts/MenuManager.cs
+++ Assets/Menu/Scripts/MenuManager.cs
@@ -5,9 +5,11 @@ using UnityTest;
 
 public class MenuManager : MonoBehaviour
 {
-
     public GameObject MainMenu;
     public GameObject OptionMenu;
+    public GameObject CustomCarMenu;
+    public GameObject CustomCharMenu;
+    public GameObject CustomAnimalsMenu;
 
     private Singleton instanceJeu;
 
@@ -17,6 +19,9 @@ public class MenuManager : MonoBehaviour
         /*active le menu à afficher au demarrage, desactive les autres */
         MainMenu.SetActive(true);
         OptionMenu.SetActive(false);
+        CustomCarMenu.SetActive(false);
+        CustomCharMenu.SetActive(false);
+        CustomAnimalsMenu.SetActive(false);
 
         instanceJeu = Singleton.DonnerInstance;
     }
@@ -26,15 +31,50 @@ public class MenuManager : MonoBehaviour
     {
         MainMenu.SetActive(true);
         OptionMenu.SetActive(false);
+        CustomCarMenu.SetActive(false);
+        CustomCharMenu.SetActive(false);
+        CustomAnimalsMenu.SetActive(false);
     }
 
     public void AfficherOptionMenu()
     {
         MainMenu.SetActive(false);
+        CustomCarMenu.SetActive(false);
+        CustomCharMenu.SetActive(false);
+        CustomAnimalsMenu.SetActive(false);
         OptionMenu.SetActive(true);
     }
 
-   public void QuitterJeu()
+    public void AfficherCustomCarMenu()
+    {
+        MainMenu.SetActive(false);
+        CustomCarMenu.SetActive(true);
+        CustomCharMenu.SetActive(false);
+        CustomAnimalsMenu.SetActive(false);
+        OptionMenu.SetActive(false);
+    }
+
+
+    public void AfficherCustomAnimalMenu()
+    {
+        MainMenu.SetActive(false);
+        CustomCarMenu.SetActive(false);
+        CustomCharMenu.SetActive(false);
+        CustomAnimalsMenu.SetActive(true);
+        OptionMenu.SetActive(false);
+    }
+
+
+    public void AfficherCustomCharMenu()
+    {
+        MainMenu.SetActive(false);
+        CustomCarMenu.SetActive(false);
+        CustomCharMenu.SetActive(true);
+        CustomAnimalsMenu.SetActive(false);
+        OptionMenu.SetActive(false);
+    }
+
+    public void QuitterJeu()
    {
         Debug.Log("Quitter");
         Application.Quit(0);
@@ -42,8 +82,13 @@ public class MenuManager : MonoBehaviour
 
     public void LancerJeu()
     {
+        Debug.Log("test");
         instanceJeu.DonnerNumeroDuNiveau = (int)Jeu.STATES.GAME;
         GameVar.DonnerInstance().GamePlayState = GameVar.GAME_STATES.GAME_STATES_START;
+        OptionMenu.SetActive(false);
+        CustomCarMenu.SetActive(false);
+        CustomCharMenu.SetActive(false);
+        CustomAnimalsMenu.SetActive(false);
     }
 
 }
